12b0e10f6b7899845505dce9f534d8646b16288a
Ludde86 <bslbjorn@gmail.com>
2020-06-12 14:30:51 +0200

    fix load users

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import TodoForm from '../todo/TodoForm';
 import TodoList from '../todo/TodoList';
+import AuthContext from '../../context/auth/authContext';
 
 const Home = () => {
+	const authContext = useContext(AuthContext);
+	const { loadUser } = authContext;
+
+	// validate and put user into state
+	useEffect(() => {
+		loadUser();
+		// eslint-disable-next-line
+	}, []);
+
 	return (
 		<div className="todo-container">
 			<TodoForm />
--- client/src/components/pages/Start.js
+++ client/src/components/pages/Start.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import PostForm from '../post/PostForm';
 import PostList from '../post/PostList';
+import AuthContext from '../../context/auth/authContext';
 
 const Start = () => {
+	const authContext = useContext(AuthContext);
+	const { loadUser } = authContext;
+
+	// validate and put user into state
+	useEffect(() => {
+		loadUser();
+		// eslint-disable-next-line
+	}, []);
+
 	return (
 		<div>
 			<PostForm />
