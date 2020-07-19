eb72d7eba8cbcdbe7223300172b927f6c561cb6c
Ludde86 <bslbjorn@gmail.com>
2020-07-19 13:57:41 +0200

    fix form position

--- client/src/components/pages/Shopping.js
+++ client/src/components/pages/Shopping.js
@@ -21,33 +21,10 @@ const Shopping = (props) => {
 	);
 
 	return (
-		<div>
-			<div className="shopping-container">
-				{/*<h3 className="shopping-title">Att Handla</h3>*/}
-				<ShoppingForm />
-				<ShoppingList />
-			</div>
-			{colorWhite && (
-				<div className="shopping-container__white">
-					{/*<h3 className="shopping-title">Att Handla</h3>*/}
-					<ShoppingForm />
-					<ShoppingList />
-				</div>
-			)}
-			{colorBlue && (
-				<div className="shopping-container__blue">
-					{/*<h3 className="shopping-title">Att Handla</h3>*/}
-					<ShoppingForm />
-					<ShoppingList />
-				</div>
-			)}
-			{colorPink && (
-				<div className="shopping-container__pink">
-					{/*<h3 className="shopping-title">Att Handla</h3>*/}
-					<ShoppingForm />
-					<ShoppingList />
-				</div>
-			)}
+		<div className="shopping-container">
+			{/*<h3 className="shopping-title">Att Handla</h3>*/}
+			<ShoppingForm />
+			<ShoppingList />
 		</div>
 	);
 };
